23a1472555da13ae491f554f3dadc67f4927bfac
Noman5237 <anonyman637@gmail.com>
2023-11-03 23:03:28 +0600

    feat(template): configured grafana prometheus tempo with applications

--- cluster/csi/create-gcs-sc.sh
+++ cluster/csi/create-gcs-sc.sh
@@ -1,3 +1,4 @@
 kubectl apply -k gcs
 kubectl create secret generic csi-gcs-secret-mounter --from-file=key=credentials/gcp-credentials.json
 kubectl create secret generic csi-gcs-secret-creator --from-file=key=credentials/gcp-credentials.json
+kubectl apply -f gcp/storageclass.yaml
